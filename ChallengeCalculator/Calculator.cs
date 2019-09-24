91e9c137c7383714a3cb2b2414feae3f3a57d0f9
Andrew <aamichel2@gmail.com>
2019-09-24 14:02:26 -0700

    Implemented requirement 2
    removed upper bound restriction in calculator 
    removed unnecessary unit tests

--- ChallengeCalculator/Calculator.cs
+++ ChallengeCalculator/Calculator.cs
@@ -8,7 +8,6 @@ namespace ChallengeCalculator
 {
     public static class Calculator
     {
-        public static int MaxNumbers = 2;
         /// <summary>
         /// Adds the numbers in the array 
         /// </summary>
@@ -16,11 +15,6 @@ namespace ChallengeCalculator
         /// <returns>Returns sum of numbers</returns>
         public static int Add(int[] numbers)
         {
-            if (numbers.Length > MaxNumbers)
-            {
-                throw new Exception($"Only a max of {MaxNumbers} allowed");
-            }
-
             int sum = 0;
             foreach(var number in numbers)
             {
--- ChallengeCalculatorTests/CalculatorTests.cs
+++ ChallengeCalculatorTests/CalculatorTests.cs
@@ -11,21 +11,11 @@ namespace ChallengeCalculatorTests
     [TestFixture]
     public class CalculatorTests
     {
-        [Test]
-        public void Add_OverMaximumAllowed_ThrowsException()
-        {
-            int[] input = new int[]
-            {
-                1, 2, 3
-            };
-
-            Assert.Throws<Exception>(() => Calculator.Add(input), $"Only a max of {Calculator.MaxNumbers} allowed");
-        }
-
         [TestCase(3, new int[] { 1, 2 })]
         [TestCase(153, new int[] { 55, 98 })]
         [TestCase(1, new int[] { 0, 1 })]
-        public void Add_UnderMaximumAllowed_ReturnsSum(int expectedSum, int[] input)
+        [TestCase(78, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 })]
+        public void Add_AllValidNumbers_ReturnsSum(int expectedSum, int[] input)
         {
             var result = Calculator.Add(input);
 
